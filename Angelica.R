a0916295640219888c07988f82acaabdf6961435
angelicaaa37 <angelicamasa081@gmail.com>
2021-07-05 09:13:27 +0800

    Personal information

---
+++ Angelica.R
@@ -0,0 +1,4 @@
+print("My name is Angelica T. Masa.")
+print("I am 19 years old and I live in Alaminos, Laguna.")
+print("I was born on San Pablo City, Laguna on December 3, 2001.")
+print("I love watching Korean dramas.")
